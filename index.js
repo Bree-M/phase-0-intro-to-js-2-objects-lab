44a7aff7a8d61028d5f37fb99e8008c606c321bc
Bree-M <brenda.wairimu@student.moringaschool.com>
2025-03-23 02:25:25 +0300

    still learning through practice

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Write your solution in this file!
+const employee = {
+   name: "Angelina",
+   streetAddress: "061101 agakhan"
+};
+
+function updateEmployeeWithKeyAndValue(employee,key,value){
+    return {...employee,[key]: value};
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+
+  function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+  
+  
+  console.log(updateEmployeeWithKeyAndValue(employee, "position", "Manager")); 
+
+  
+  console.log(employee); 
+  
+  
+  console.log(destructivelyUpdateEmployeeWithKeyAndValue(employee, "position", "Manager")); 
+  
+  
+  console.log(deleteFromEmployeeByKey(employee, "streetAddress")); 
+  
+  
+  console.log(employee); 
+  
+  
+  console.log(destructivelyDeleteFromEmployeeByKey(employee, "streetAddress")); 
+  
+  
+  console.log(employee);
